a04fe3b50199cc5cf4f34645a7031c297b8cbdc1
Elsa Weber <elsa.weber0@gmail.com>
2025-01-25 11:50:26 +0100

    Liste mieux

--- scenes/composition/composition.gd
+++ scenes/composition/composition.gd
@@ -2,14 +2,25 @@ extends Node
 
 signal validate
 
-static var CHOSEN_TEA: String = "green_tea"
+static var POSSIBLE_TEAS: Array = [
+	{"type": "green", "title": "Green Tea", "descr": "Blabla green"},
+	{"type": "black", "title": "Black Tea", "descr": "Blabla black"},
+	{"type": "green", "title": "Maté", "descr": "Blabla maté"},
+]
+	
 static var CHOSEN_SIRUP: String = "peach"
 static var CHOSEN_BUBBLES: String = "strawberry"
+
+const scene_item = "res://scenes/composition/icon_item_container.tscn"
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	for tea in POSSIBLE_TEAS:
+		var item = preload(scene_item).instantiate()
+		item.item_title = tea["title"]
+		item.item_descr = tea["descr"]
+		$TypeSelection/List.add_child(item)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -17,5 +28,5 @@ func _process(delta: float) -> void:
 	pass
 
 
-func _on_button_pressed() -> void:
-	validate.emit({"tea": CHOSEN_TEA, "sirup": CHOSEN_SIRUP, "bubbles": CHOSEN_BUBBLES})
+#func _on_button_pressed() -> void:
+	#validate.emit({"tea": CHOSEN_TEA, "sirup": CHOSEN_SIRUP, "bubbles": CHOSEN_BUBBLES})
--- scenes/composition/icon_item_container.gd
+++ scenes/composition/icon_item_container.gd
@@ -1,36 +1,34 @@
-extends HBoxContainer
+extends TextureRect
 
-#@onready var icon: TextureRect = %Icon
-#@onready var title: Label = %Title
-#@onready var description: Label = %Description
+@onready var icon: TextureRect = %Icon
+@onready var title: Label = %Title
+@onready var description: Label = %Description
 
-@export var item_icon: Texture
-	#set(value):
-		#item_icon = value
-		#_update_labels()
+@export var item_icon: Texture:
+	set(value): 
+		item_icon = value
+		_update_labels()
 
-@export var item_title: String = "Title"
-	#set(value):
-		#item_title = value
-		#_update_labels()
+@export var item_title: String = "Title":
+	set(value):
+		item_title = value
+		_update_labels()
 		
-@export var item_descr: String = "Description"
-	#set(value):
-		#item_descr = value
-		#_update_labels()
+@export var item_descr: String = "Description":
+	set(value):
+		item_descr = value
+		_update_labels()
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	$Icon.texture = item_icon
-	$Title.text = item_title
-	$Description.text = item_descr
+	_update_labels()
 	
-#func _update_labels():
-	#if icon:
-		#icon.texture = item_icon
-	#if title:
-		#title.text = item_title
-	#if description:
-		#description.text = item_descr
-	
+func _update_labels():
+	if icon:
+		icon.texture = item_icon
+	if title:
+		print("---")
+		title.text = item_title
+	if description:
+		description.text = item_descr
